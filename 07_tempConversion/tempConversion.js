206bf90249c3cabeb02f1f84f243a4caa82bc173
Raul Resendez <raul.resendez@proton.me>
2024-09-26 13:34:58 -0500

    complete exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+
+  const celsius = (5/9)*(temp-32);
+
+  const rounded = Math.round(celsius * 10) / 10;
+
+  return rounded;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+
+  const farenheit = ((9/5)*temp)+32;
+  const rounded = Math.round(farenheit * 10) / 10;
+
+  return rounded;
 };
 
 // Do not edit below this line
