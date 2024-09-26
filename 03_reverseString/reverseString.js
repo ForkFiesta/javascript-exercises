78e33e89970ff0bbb63200ddb4cb58ebfc460bdf
Raul Resendez <raul.resendez@proton.me>
2024-09-26 06:49:52 -0500

    complete excercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,20 @@
-const reverseString = function() {
+const reverseString = function(word) {
+
+    const wordArray = word.split("")
+    const reverseArray = []
+    for (i = wordArray.length-1; i>=0; i--)
+        {
+            reverseArray.push(wordArray[i]);
+        }
+
+    let reverseWord = reverseArray.join("");
+
+    return reverseWord;
+    
+
 
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
