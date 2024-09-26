78206f9c30c8726ae2249e29dc76428e2a27dd6e
Raul Resendez <raul.resendez@proton.me>
2024-09-26 13:05:04 -0500

    Complete exercise 5 - ugly conditionals but works

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,49 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    let largeNum;
+    let smallNum;
+
+    if (typeof(num1)!= "number" || typeof(num2)!= "number")
+        {return "ERROR"};
+
+    if (num1 % 1 != 0 || num2 % 1 != 0)
+        {
+        return "ERROR";
+        }
+
+
+    if (num1<0 || num2<0)
+        {
+            return "ERROR";
+        }
+
+    if (num1 > num2)
+        {
+            largeNum = num1;
+            smallNum = num2;
+        }
+        else
+        {
+            smallNum = num1;
+            largeNum = num2;
+        }
+
+    const largeArray = [...Array(largeNum+1).keys()];
+    let index = largeArray.indexOf(smallNum)
+    // dumpNum = largeArray.shift()
+    // let index = smallNum + 1;
+    const sumArray = largeArray.slice(index);
+
+    for (i = 0; i <sumArray.length; i++)
+    {
+        sum+=sumArray[i];
+    }
+
+    return  sum;
+
+        
+
+
 
 };
 
