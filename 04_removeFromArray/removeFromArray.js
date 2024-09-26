b9478b1c2b38b5c5cfb09f0e10399eb2f4252df6
Raul Resendez <raul.resendez@proton.me>
2024-09-26 10:55:15 -0500

    Completed exercise 3. Used While loop, index of and splice

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,29 @@
-const removeFromArray = function() {
+const removeFromArray = function(someArray, ...args) {
+
+    originalArray = someArray;
+    valuesToRemove = args;
+
+    for (const value of valuesToRemove)
+        {
+            let index = originalArray.indexOf(value);
+            while (index !== -1)
+                {
+                    originalArray.splice(index, 1);
+                    index = originalArray.indexOf(value);
+                }
+
+        }
+
+
+    return originalArray;
+
+
+
+
+
 };
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
