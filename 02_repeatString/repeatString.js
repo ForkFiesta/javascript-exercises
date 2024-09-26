7de154bedda80ca2a5c1d2056ead5ba116613c6a
Raul Resendez <raul.resendez@proton.me>
2024-09-26 06:40:17 -0500

    Complete 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,23 @@
-const repeatString = function() {
+const repeatString = function(word, num) {
 
+    
+    wordSalad = [];
+    if (num >= 0)
+        {
+            for (i = 0; i < num; i++)
+                {
+                    wordSalad.push(word);
+                }
+            
+            let repeatedString = wordSalad.join('')
+        
+            return repeatedString
+        
+        }
+        else {
+            return 'ERROR';
+        }
+    
 };
 
 // Do not edit below this line
